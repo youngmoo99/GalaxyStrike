2e365e0ea50a6b4eb31ae0249d48f7003a2ebe87
YoungMoo Kim <rladuan612@gmail.com>
2025-07-15 21:02:29 +0900

    Update Scoreboard

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,9 +3,28 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {   
     [SerializeField] GameObject destroyedVFX;
+    [SerializeField] int hitPoints = 3;
+    [SerializeField] int scoreValue = 10;
+
+    Scoreboard scoreboard; //점수 
+
+    void Start()
+    {
+        scoreboard = FindFirstObjectByType<Scoreboard>();
+    }
     void OnParticleCollision(GameObject other) //Non-trigger 콜라이더가있는 게임오브젝트에 부착될때 호출
     {   
-        Instantiate(destroyedVFX, transform.position, Quaternion.identity);
-        Destroy(this.gameObject);
+        ProcessHit();
+    }
+
+    void ProcessHit()// 적 hp 감소
+    {
+        hitPoints--;
+        if(hitPoints <= 0) //3번 적중시 파괴
+        {   
+            scoreboard.IncreaseScore(scoreValue);
+            Instantiate(destroyedVFX, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using TMPro;
+
+public class Scoreboard : MonoBehaviour
+{   
+    [SerializeField] TMP_Text scoreboardText;
+    int score = 0;
+
+    public void IncreaseScore(int amount)
+    {
+        score += amount;
+        scoreboardText.text = score.ToString();
+    }
+
+
+}
